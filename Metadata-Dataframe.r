ed8f21a0f767b0bae5fe525e34419715a7b9c188
Hugh Raggett <hcr64@nau.edu>
2025-07-21 07:46:18 +0000

    First commit 7/21, added notes and file paths

--- Metadata-Dataframe.r
+++ Metadata-Dataframe.r
@@ -1,5 +1,16 @@
 # import proper packages 
 
-# get an array of the file names in the directory
 
-# 
+# get a path to the directory of metadata files
+dir_path <- "Home/data-store/data/iplant/home/shared/geri/metadata/saeon_metadata/"
+
+# get a list of the file names in the directory
+
+# create the dataframe
+
+# for each file in the directory, 
+  # take the xml data from the file and get a dict struct
+
+  # add that struct to the larger dataframe 
+
+# save the dataframe somewhere
