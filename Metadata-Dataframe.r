de89e47939fdcf50b15915f23587d17f2604d4c8
Hugh Raggett <hcr64@nau.edu>
2025-07-24 23:16:48 +0000

    added printing the dataframe to a file, and adds values to the dataframe

--- Metadata-Dataframe.r
+++ Metadata-Dataframe.r
@@ -1,17 +1,28 @@
 # import proper packages 
 library(XML)
 
-# a function that gets the information from a root in an xml file
-# if the root has more children, it recursively calls itself until there are none
-# its only arguement is an xml root
-xml_root_gatherer <- function( xml_root )
-{
-  # get the number of children in the root 
-  num_roots <- sum( sapply( xmlChildren( xml_root ), xmlName ) != "" )
+# get a path to the directory of metadata files
+dir_path <- "data-store/data/iplant/home/shared/geri/geri-harmonized/metadata/saeon_metadata/"
 
-  # make a list to gether the data for
+# where to print the dataframe
+print_df_path = "Geri-Metadata-df-Maker/saeon_output.csv"
+
+# get a list of the file names in the directory
+list_of_files <- list.files( path=dir_path )
+print( list_of_files )
+
+# a function to scrape all the data from an XMl file,
+# and returns a named list of the data inside
+# pass the root of the file as an arg
+scrape_xml <- function( xml_root )
+{
+  # get the number of roots in the file
+  num_roots <- sum( sapply( xmlChildren( xml_root ), xmlName ) != "" )
   
-  # iterate through the roots
+  #the array storing the data with the keys being the xml tags
+  root_val_arr <- list()
+  
+  # iterate through the roots in the file
   for( i in 1:num_roots )
   {
     # get the name of the root
@@ -20,24 +31,19 @@ xml_root_gatherer <- function( xml_root )
     # get the root value
     root_value <- xmlValue( xml_root[[i]] )
     
-    # look at the roots
-    print( cat( root_name, root_value ) )
+    # add the value to the list using the root
+    root_val_arr[[ root_name ]] <- root_value
     
     # if the root is a subroot,
-      # call the function again
-  
+    # call the function again
+    
     # otherwise,
-      # add it to the list of data
+    # add it to the list of data
   }
   
-  # return the list
+  # return the array
+  root_val_arr
 }
-
-# get a path to the directory of metadata files
-dir_path <- "data-store/data/iplant/home/shared/geri/metadata/saeon_metadata/"
-
-# get a list of the file names in the directory
-list_of_files <- list.files( path=dir_path )
 
 # create the dataframe
 df <- data.frame()
@@ -45,16 +51,18 @@ df <- data.frame()
 # for each file in the directory,
 for (file in list_of_files)
 {
-  print( file )
   # read the file as an xml 
   xml_data <- xmlParse( paste( dir_path, file, sep="" ) )
   
   # get the root of the xml data
   root <- xmlRoot( xml_data )
   
-  xml_root_gatherer( root )
+  # scrape the xml file and get the data in a named list
+  val_arr <- scrape_xml( root )
   
-  # add that struct to the larger dataframe 
+  # add that struct to the larger dataframe
+  df <- rbind( df, val_arr )
 }
 
 # save the dataframe somewhere
+write.csv( df, print_df_path )
