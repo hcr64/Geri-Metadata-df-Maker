36f9ac095f5c21f1053e7c7fd82bff77682c8aa6
hcr64 <149124502+hcr64@users.noreply.github.com>
2025-07-31 15:33:26 -0700

    Update Scrape-xml.R

--- Scrape-xml.R
+++ Scrape-xml.R
@@ -1,6 +1,9 @@
 # include the xml library
 library(XML)
 
+# include my subroot scraper function
+source("Geri-Metadata-df-Maker/Scrape-subroot.R")
+
 # a function to scrape all the data from an XMl file,
 # and returns a named list of the data inside
 # pass the root of the file as an arg
@@ -15,20 +18,27 @@ scrape_xml <- function( xml_root )
   # iterate through the roots in the file
   for( i in 1:num_roots )
   {
+    # get the current root 
+    curr_root <- xml_root[[i]]
+    
     # get the name of the root
-    root_name <- xmlName( xml_root[[i]] ) 
+    root_name <- xmlName( curr_root ) 
     
-    # get the root value
-    root_value <- xmlValue( xml_root[[i]] )
+    # if the root has a subroot,
+    if( sum( sapply( xmlChildren( curr_root ), xmlName ) != "" ) > 1 )
+    {
+      # call the function again, and store result as a string
+      root_value <- scrape_subroot( curr_root, divider="##" )
+    }
     
-    # add the value to the list using the root
+    # otherwise just set the root_value var to the value
+    else
+    {
+      root_value <- xmlValue( xml_root[[i]] )
+    }
+    
+    # add the data as a string to the list of data
     root_val_arr[[ root_name ]] <- root_value
-    
-    # if the root is a subroot,
-    # call the function again
-    
-    # otherwise,
-    # add it to the list of data
   }
   
   # return the array
